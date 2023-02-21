83fd7022e58b88ddeb1db1f8bc1efd4776c18250
Dez <dzelwj@outlook.com>
2023-02-21 14:39:10 +0800

    Improve scripts

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -2,29 +2,35 @@
 import os
 
 if __name__ == '__main__':
-    data_dir = './data/SNAP_modified/'
-    output_dir = './outputs/'
+    kl_set = [(2, 2)]
     bin_dir = './build/bin'
-    algo_exe = './build/bin/algo_on_server'
-    algo2_exe = './build/bin/algo2_on_server'
-    bf_exe = './build/bin/bf_on_server'
-    bf2_exe = './build/bin/bf2_on_server'
+    data_dir = './data/SNAP_modified'
+    output_dir = './outputs'
     output_bound = 300
-    running_time_bound = 300 # seconds
+    runtime_bound = 300
 
-    for filename in os.listdir(data_dir):
-        for k, l in [(2, 2), (2, 3), (3, 3)]:
-            for exe in [algo_exe, algo2_exe, bf_exe, bf2_exe]:
-                output_filename = exe[12:-6] + "k{}l{}b{}_".format(k, l, output_bound) + filename
-                command = "{} {} {} {} {} {} > {}{}".format(
-                    exe,
-                    data_dir + filename,
-                    output_bound,
-                    running_time_bound,
+    for data_file in os.listdir(data_dir):
+        if not data_file.endswith('.txt'):
+            continue
+        for k, l in kl_set:
+            for prog in os.listdir(bin_dir):
+                output_file = "{}_k{}l{}c{}t{}_{}.txt".format(
+                    os.path.splitext(data_file)[0],
                     k,
                     l,
-                    output_dir,
-                    output_filename)
+                    output_bound,
+                    runtime_bound,
+                    os.path.splitext(prog)[0]
+                )
+                command = "{} {} {} {} {} {} > {}".format(
+                    os.path.join(bin_dir, prog),
+                    os.path.join(data_dir, data_file),
+                    output_bound,
+                    runtime_bound,
+                    k,
+                    l,
+                    os.path.join(output_dir, output_file)
+                )
                 print("Running " + command + " ...")
                 os.system(command)
                 print("Done")
